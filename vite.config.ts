5a8b6dd4996ed92fce0622d5279a7aab8c51a455
Varun <vrun82000@gmail.com>
2025-12-14 18:46:26 +0530

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
+    base: '/Civvi/',
     plugins: [react()],
     define: {
       // Polyfill process.env for the existing code structure
